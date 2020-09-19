d3baeaca64d4030eeb52bb5c72ab017d35670f32
hoodielive <hoodielive@gmail.com>
2020-09-19 14:35:51 -0700

    Added scripts.

---
+++ infra/ansible/scripts/case.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+echo -n "Enter the name of a state: "
+
+read STATE 
+
+echo -n "The capital city of $STATE is: "
+
+case $STATE in 
+	Georgia)
+		echo "Atlanta"
+		;;
+	Virginia)
+		echo "Richmond"
+		;;
+	Texas)
+		echo "Austin"
+		;;
+	Maine)
+		echo "Augusta"
+		;;
+	*)
+		echo "Not in database."
+		;;
+esac
