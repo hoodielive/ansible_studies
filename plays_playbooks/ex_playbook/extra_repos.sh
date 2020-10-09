72424e8a46dbdef11c113483b4b1c03f3c2d3ade
hoodielive <hoodielive@gmail.com>
2020-10-08 23:27:03 -0400

    Add script to show the differences between script and playbook (yaml).

---
+++ plays_playbooks/ex_playbook/extra_repos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+# Install Epel Repo.
+
+yum install -y epel-release 
+
+# Import REMI GPG key.
+
+wget https://rpms.remirepo.net/RPM-GPG-KEY-remi  -O /etc/pki/rpm-gpg/RPM-GPG-KEY-remi
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-remi
+
+# Install Remi repo.
+
+rpm -Uvh --quiet https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+# Install Node.js (npm plus all its dependencies).
+
+yum --enablerepo=epel -y install npm
